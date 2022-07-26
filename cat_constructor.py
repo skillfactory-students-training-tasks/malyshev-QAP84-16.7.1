8f6df7ec84d19e756ab0034a4ee1f780cbf89023
d0nny113 <d0nny13051990@gmail.com>
2022-07-26 23:02:15 +0530

    kitty kitty kitty

---
+++ cat_constructor.py
@@ -0,0 +1,9 @@
+class cat:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def cat_info(self):
+        print(f"Имя: {self.name} Возраст: {self.age} Пол: {self.gender}")
+
---
+++ cat_printerr.py
@@ -0,0 +1,8 @@
+from cat_constructor import cat
+
+tom = cat("Том",6,"мальчик")
+grfld = cat("Гарфилд",4,"мальчик")
+lilit = cat("Лилит",999,"девочка")
+
+cats = (tom.cat_info(), grfld.cat_info(), lilit.cat_info())
+
